84251e1092954f86dd5e57412569848873609499
Tony <TechnoToneUK@live.co.uk>
2015-01-13 10:47:01 +0000

    First commit.

---
+++ photos/Create Thumbs.ps1
@@ -0,0 +1,93 @@
+﻿cls
+$sourceDir = Split-Path $PSCommandPath -Parent
+$destDir = Join-Path $sourceDir "thumbs"
+$thumbHeight = 65
+
+function main() {
+
+    Add-Type -Assembly System.Drawing
+
+    emptyThumbsDirectory
+
+    getImages | % {createThumb $_}
+
+}
+
+function emptyThumbsDirectory() {
+    Get-ChildItem -File -Path $destDir | Remove-Item
+}
+
+function getImages() {
+    Get-ChildItem -File -Recurse -Path $sourceDir -Include "*.jpg"
+}
+
+function createThumb() {
+    param (
+        [System.IO.FileInfo] $sourceFile
+    )
+
+    Write-Output $sourceFile.Name
+
+    $destFile = Join-Path $destDir (Split-Path $sourceFile -Leaf)
+    $original = [System.Drawing.Bitmap]::FromFile($sourceFile.FullName)
+
+    $thumbWidth = [int]($original.Width / $original.Height * $thumbHeight)
+
+    $orientation = $original.PropertyItems | where {$_.Id -match "112|274|5029"}
+    if ($orientation) {
+        $original.RemovePropertyItem($orientation.Id)
+        
+        $orientation = $orientation.Value[0]
+
+        if ($orientation -ne 1) {
+            if ($orientation -eq 6) {
+                $original.RotateFlip([System.Drawing.RotateFlipType]::Rotate90FlipNone)
+            } elseif ($orientation -eq 8) {
+                $original.RotateFlip([System.Drawing.RotateFlipType]::Rotate270FlipNone)
+            } elseif ($orientation -eq 3) {
+                $original.RotateFlip([System.Drawing.RotateFlipType]::Rotate180FlipNone)
+            }
+
+            $newFile = $sourceFile.FullName -replace ".jpg",".new.jpg"
+            $original.Save($newFile)
+            $original.Dispose()
+        
+            $sourceFile.Delete()
+            Get-Item -Path $newFile | Move-Item -Destination $sourceFile.FullName
+
+            $original = [System.Drawing.Bitmap]::FromFile($sourceFile.FullName)
+            $thumbWidth = [int]($original.Width / $original.Height * $thumbHeight)
+        }
+    }
+
+    #$thumb = New-Object -TypeName System.Drawing.Bitmap($thumbWidth, $thumbHeight)
+    #$graphics = [System.Drawing.Graphics]::FromImage($thumb)
+    #$graphics.DrawImage($original, 0, 0, $thumbWidth, $thumbHeight);
+    #$graphics.Dispose();
+
+    $thumb = $original.GetThumbnailImage($thumbWidth, $thumbHeight, $null, [intptr]::Zero)
+
+    $thumb.Save($destFile)
+}
+
+
+#$source = "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+#$destination = "$Home\Pictures\thumb0.png"
+#$scale = 0.25
+
+
+## Open and resize the image
+#$image = New-Object System.Windows.Media.Imaging.TransformedBitmap (New-Object System.Windows.Media.Imaging.BitmapImage $source),
+#                                                                   (New-Object System.Windows.Media.ScaleTransform $scale,$scale)
+## Put it on the clipboard (just for fun)
+#[System.Windows.Clipboard]::SetImage( $image )
+
+## Write out an image file:
+#$stream = [System.IO.File]::Open($destination, "OpenOrCreate")
+#$encoder = New-Object System.Windows.Media.Imaging.$([IO.Path]::GetExtension($destination).substring(1))BitmapEncoder
+#$encoder.Frames.Add([System.Windows.Media.Imaging.BitmapFrame]::Create($image))
+#$encoder.Save($stream)
+#$stream.Dispose()
+
+
+main
---
+++ scripts/custom.js
@@ -0,0 +1,248 @@
+﻿$(function() {
+
+  var contentContainer = $("#content-container");
+  var photographs = null;
+  var slideshowObj = null;
+
+  contentContainer.fadeOut(1);
+  $(".fade-in").fadeIn(1000);
+
+  function getPageName() {
+    var pageName = window.location.pathname;
+    if (pageName.slice(-1) === "/") {
+      pageName = pageName.substring(0, pageName.length - 1);
+    }
+    pageName = pageName.split("/").pop();
+
+    if (pageName === "") pageName = "Home.html";
+    return pageName;
+  }
+
+  function initKenBurns() {
+    jQuery(function () {
+      jQuery("#bannerscollection_zoominout_generous")
+        .bannerscollection_zoominout({
+          skin: "opportune",
+          responsive: false,
+          absUrl: "/css/",
+          width: 900,
+          height: 400,
+          width100Proc: false,
+          height100Proc: false,
+          autoPlay: 8,
+          enableTouchScreen: false,
+          pauseOnMouseOver: false,
+          horizontalPosition: "center",
+          verticalPosition: "center",
+          initialZoom: 1,
+          finalZoom: 0.5,
+          duration: 10,
+          thumbsOnMarginTop: 0,
+          thumbsWrapperMarginTop: -10,
+          showPreviewThumbs: false,
+          showCircleTimer: false,
+          showNavArrows: false,
+          showBottomNav: false,
+          autoHideBottomNav: false,
+          zoomEasing: "easeOutSine",
+          callback: function(obj) {
+            slideshowObj = obj;
+          }
+        });
+    });
+  }
+
+  function populateGallery(photographs) {
+    var galleryList = $(".bannerscollection_zoominout_list");
+    if (!galleryList) return;
+    galleryList.html("");
+    var galleryTexts = $(".bannerscollection_zoominout_text_container");
+    galleryTexts.html("");
+
+    for (var i in photographs) {
+      if (photographs.hasOwnProperty(i)) {
+        var photo = photographs[i];
+
+        var newHtml = "<li " +
+          "data-initialzoom=\"" + photo.initialzoom + "\" " +
+          "data-finalzoom=\"" + photo.finalzoom + "\" " +
+          "data-horizontalposition=\"" + photo.horizontalposition + "\" " +
+          "data-verticalposition=\"" + photo.verticalposition + "\" " +
+          "data-bottom-thumb=\"/photos/thumbs/" + photo.filename + "\" " +
+          "data-text-id=\"#photo_description_" + i.toString() + "\" " +
+          "><img src=\"/photos/" + photo.filename + "\" " +
+          "style=\"width: " + photo.width + "px; height: " + photo.height + "px;\" " +
+          "alt=\"\" /></li>";
+
+        galleryList.append(newHtml);
+
+        newHtml = "<div " +
+          "id=\"photo_description_" + i + "\" " +
+          "class=\"bannerscollection_zoominout_texts\">" +
+          "<div class=\"bannerscollection_zoominout_text_line textElement11_generous\"" +
+          "data-initial-left=\"-50\"" +
+          "data-initial-top=\"0\"" +
+          "data-final-left=\"20\"" +
+          "data-final-top=\"20\"" +
+          "data-duration=\"0.5\"" +
+          "data-fade-start=\"0\"" +
+          "data-delay=\"0.23\">" +
+          "<div class=\"phototitle\">" + photo.description + "</div></div>";
+
+        galleryTexts.append(newHtml);
+      }
+    }
+  }
+
+  function loadGallery() {
+    if (photographs == null || photographs.length === 0) {
+
+      $.getJSON("/photos/__list.json",  function ( data) {
+        if (data !== null) {
+          photographs = data;
+        }
+
+        populateGallery(photographs);
+        initKenBurns();
+      });
+    } else {
+      populateGallery(photographs);
+      initKenBurns(photographs);
+    }
+  }
+
+  function initCarousel() {
+    $('[data-ride = "carousel"]').each(function() {
+      var $carousel = $(this);
+      $carousel.carousel($carousel.data());
+    });
+  }
+
+  function showTextValue(identifier, value) {
+    var element = $(identifier);
+    if (element != null) {
+      element.html(value);
+    }
+  }
+
+  function doPageAnimations() {
+    showTextValue("#email-address", 'dhcarpentry@outlook.com');
+    showTextValue("#telephone-number", "07748 151682");
+
+    //$(".switch").addClass("on");
+    var count = $(".switch").length;
+    if (count > 0) {
+      count = 2;
+      $(".switch").each(function(i, e) {
+        setTimeout(function() {
+          $(e).addClass("on");
+        }, count++ * 100);
+
+      });
+    }
+  }
+
+  function doPageFadeout(faded) {
+
+    var count = $(".switch.on").length;
+
+    if (count > 0) {
+      count = 2;
+      $(".switch.on").each(function(i, e) {
+        setTimeout(function() {
+          $(e).addClass("off");
+        }, count++ * 100);
+      });
+      setTimeout(function() {
+        contentContainer.fadeOut(500, function() { faded.resolve(); });
+      }, count * 100 + 300);
+    } else {
+      contentContainer.fadeOut(500, function() { faded.resolve(); });
+    }
+
+    return;
+  }
+
+  function navigateToPage() {
+
+    if (slideshowObj) {
+      slideshowObj.abort = true;
+      slideshowObj = null;
+    }
+
+    var pageName = getPageName();
+
+    var faded = $.Deferred();
+    var geted = $.Deferred();
+    var fragment = null;
+
+    doPageFadeout(faded);
+
+    if (pageName.slice(-1) === "\\") {
+      geted.resolve();
+    } else {
+      $.get("/" + pageName, function(response) {
+
+        var markup = $("<div>" + response + "</div>");
+        fragment = markup.find("#content-container").html();
+
+        geted.resolve();
+      });
+    }
+
+    $.when(faded, geted).done(function() {
+      var container = contentContainer;
+
+      if (fragment !== null) {
+        container.html(fragment);
+      }
+
+      container.fadeIn(300, function() {
+        doPageAnimations();
+        initCarousel();
+        loadGallery();
+      });
+    });
+  }
+
+  photographs = [];
+  $("a[data-role = 'ajax']").click(function(e) {
+    $(".navbar-nav").children().removeClass('active');
+    $(this.parentElement).addClass('active');
+
+    if (Modernizr.history) {
+      e.preventDefault();
+      var pageName = $(this).attr("href");
+      window.history.pushState(null, "", pageName);
+      navigateToPage();
+    }
+  });
+
+  $(".navbar-brand").click(function(e) {
+    $(".navbar-nav").children().removeClass("active");
+    $($(".navbar-nav").children()[0]).addClass("active");
+
+    if (Modernizr.history) {
+      e.preventDefault();
+      window.history.pushState(null, "", "/");
+      navigateToPage();
+    }
+  });
+
+
+  var firstTime = true;
+
+  $(window).on("popstate", function(e) {
+
+    if (firstTime) {
+      firstTime = false;
+      if ($.browser && $.browser.webkit) {
+        return;
+      }
+    }
+
+    navigateToPage();
+  });
+
+  navigateToPage();
+});
